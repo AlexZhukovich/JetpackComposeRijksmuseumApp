b9d89092f0b1d0461c3254d768106df688a752d7
Alex Zhukovich <zhukovich.a@gmail.com>
2025-07-11 01:05:52 +0200

    Removed unused import; Updated okhttp dependency to 5.1.0

--- app/src/main/java/com/alexzh/rijksmuseum/data/remote/RemoteArtObjectsRepository.kt
+++ app/src/main/java/com/alexzh/rijksmuseum/data/remote/RemoteArtObjectsRepository.kt
@@ -1,15 +1,14 @@
 package com.alexzh.rijksmuseum.data.remote
 
+import com.alexzh.rijksmuseum.data.mapper.toArtObject
+import com.alexzh.rijksmuseum.data.mapper.toArtObjectDetails
+import com.alexzh.rijksmuseum.data.remote.model.ArtObjectsPage
 import com.alexzh.rijksmuseum.domain.ArtObjectsRepository
 import com.alexzh.rijksmuseum.domain.Result
 import com.alexzh.rijksmuseum.domain.exception.ApiException
 import com.alexzh.rijksmuseum.domain.exception.ArtObjectNotFoundException
 import com.alexzh.rijksmuseum.domain.exception.NetworkException
 import com.alexzh.rijksmuseum.domain.exception.UnauthorizedException
-import com.alexzh.rijksmuseum.data.mapper.toArtObject
-import com.alexzh.rijksmuseum.data.mapper.toArtObjectDetails
-import com.alexzh.rijksmuseum.data.remote.model.ArtObjectsPage
-import com.alexzh.rijksmuseum.domain.model.ArtObject
 import com.alexzh.rijksmuseum.domain.model.ArtObjectDetails
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
